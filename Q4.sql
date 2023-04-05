e1ac0bf10ac3e334e5ff2908f9c8ef8d0d7e5144
ishaj1 <93831881+ishaj1@users.noreply.github.com>
2023-04-05 12:42:51 -0400

    Update Q4.sql

--- Q4.sql
+++ Q4.sql
@@ -1,7 +1,7 @@
-Select * from Flight where current_timestamp < departure_datetime;
+Select * from Flight where current_datetime < departure_datetime;
 
 Select * from Flight where status = 'delayed';
 
-Select distinct name from Customer natural join Ticket;
+Select distinct fname, lname from Customer natural join Ticket;
 
 Select airplane_id from Airplane where airline_name = 'Jet Blue'

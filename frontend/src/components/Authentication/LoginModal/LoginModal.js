e042e6f9a32bc9f6214ff3165a7668177a8512d9
Jia <jiahuang2468@gmail.com>
2023-04-08 22:06:05 -0400

    login modal

--- frontend/src/components/Authentication/LoginModal/LoginModal.js
+++ frontend/src/components/Authentication/LoginModal/LoginModal.js
@@ -1,10 +1,11 @@
-import { useEffect, useRef, useContext } from "react";
+import { useEffect, useRef, useContext, useState } from "react";
 import AuthContext from "../../../context/auth-context";
 import styles from "./LoginModal.module.css";
 
 const LoginModal = () => {
   const ctx = useContext(AuthContext);
   const modalRef = useRef(null);
+  const [isCustomer, setIsCustomer] = useState(true);
 
   useEffect(() => {
     const clickOutsideHandler = (event) => {
@@ -18,9 +19,41 @@ const LoginModal = () => {
     };
   }, [modalRef]);
 
+  const isCustomerHandler = (event) => {
+    event.stopPropagation();
+    setIsCustomer(event.target.value === "customer");
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <form className={styles.modal} ref={modalRef}>
-      Sign In
+    <form onSubmit={submitHandler} className={styles.modal} ref={modalRef}>
+      <div className={styles.type}>
+        <button
+          className={isCustomer ? styles.selected : styles.notSelected}
+          type="button"
+          value="customer"
+          onClick={isCustomerHandler}
+        >
+          Customer
+        </button>
+        <button
+          className={isCustomer ? styles.notSelected : styles.selected}
+          type="button"
+          value="staff"
+          onClick={isCustomerHandler}
+        >
+          Staff
+        </button>
+      </div>
+      <div className={styles.form}>
+        <h2>Login</h2>
+        <input placeholder={isCustomer ? "E-Mail" : "Username"} />
+        <input placeholder="Password" />
+        <button type="submit">Sign In</button>
+      </div>
     </form>
   );
 };

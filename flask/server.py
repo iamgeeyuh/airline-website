3c8d248844de6d8f55e1fca7a81174764c8ad08f
ShenyiHuang <moucangcang@gmail.com>
2023-04-19 22:05:16 -0400

    update server.py

--- flask/server.py
+++ flask/server.py
@@ -16,10 +16,14 @@ conn = pymysql.connect(host='localhost',
 def login():
   username = request.form['username'] #TODO: needs to be fetched in front end
   password = request.form['password'] #TODO:needs to be fetched in front end
+  isCustomer = request.form['isCustomer'] #TODO:needs to be fetched in front end
 
   cursor = conn.cursor
 
-  query = 'SELECT * FROM user WHERE username = %s and password = %s' #get the username and password
+  if (isCustomer):
+    query = 'SELECT * FROM Customer WHERE email = %s and password = %s' #get the username and password
+  else:
+    query = 'SELECT * FROM Staff WHERE username = %s and password = %s'
   cursor.execute(query, (username, password))
 
   data = cursor.fetchone()
@@ -31,8 +35,34 @@ def login():
     session['username'] = username
     return {"user": True} #TODO:redirect to the home page in front end for testing
   else:
-    return {"user": False}
+    return {"user": False} #TODO: display an error message (according to page6 3B)
 
+#Authenticates the register
+@app.route('/registerAuth', methods=['GET', 'POST'])
+def registerAuth():
+	#grabs information from the forms
+	username = request.form['username']
+	password = request.form['password']
+
+	#cursor used to send queries
+	cursor = conn.cursor()
+	#executes query
+	query = 'SELECT * FROM user WHERE username = %s'
+	cursor.execute(query, (username))
+	#stores the results in a variable
+	data = cursor.fetchone()
+	#use fetchall() if you are expecting more than 1 data row
+	error = None
+	if(data):
+		#If the previous query returns data, then user exists
+		error = "This user already exists"
+		return render_template('register.html', error = error)
+	else:
+		ins = 'INSERT INTO user VALUES(%s, %s)'
+		cursor.execute(ins, (username, password))
+		conn.commit()
+		cursor.close()
+		return render_template('index.html')
 
 #home page for testing
 @app.route("/home", methods=['GET', 'POST'])

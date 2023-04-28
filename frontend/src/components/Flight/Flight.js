c9d73beae2a0f252fadc3a266e4b6e4be4993243
Jia <jiahuang2468@gmail.com>
2023-04-27 21:11:27 -0400

    home

--- frontend/src/components/Flight/Flight.js
+++ frontend/src/components/Flight/Flight.js
@@ -1,7 +1,32 @@
+import styles from "./Flight.module.css";
+
 const Flight = (props) => {
-    return(
-        <p>hi</p>
-    )
+  return (
+    <div className={styles.flight}>
+      <label className={styles.flightAirline}>{props.airline}</label>
+      <div>
+        <div>
+          <label>From</label>
+          <h2>{props.depTime}</h2>
+          <h5 style={{ color: "var(--font-purple)" }}>
+            {props.srcCity}-{props.srcAirport}
+          </h5>
+          <h5>{props.depDate}</h5>
+        </div>
+        <div>
+          <label>To</label>
+          <h2>{props.arrTime}</h2>
+          <h5 style={{ color: "var(--font-purple)" }}>
+            {props.dstCity}-{props.dstAirport}
+          </h5>
+          <h5>{props.arrDate}</h5>
+        </div>
+        <div>
+          <h2>${props.price}</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Flight;
--- frontend/src/components/FoundFlight/FoundFlight.js
+++ frontend/src/components/FoundFlight/FoundFlight.js
@@ -1,7 +1,24 @@
 import Flight from "../Flight/Flight";
 
 const FoundFlight = () => {
-  return <Flight />;
+  return (
+    <div>
+      <h2>Flights Found</h2>
+      <Flight
+        airline="Jet Blue"
+        srcCity="New York City"
+        srcAirport="JFK"
+        dstCity="Boston"
+        dstAirport="Boston Airport"
+        flightNum="587"
+        depDate="2023-04-04"
+        depTime="15:00:00"
+        arrDate="2023-04-04"
+        arrTime="8:00:00"
+        price="3000.00"
+      />
+    </div>
+  );
 };
 
 export default FoundFlight;

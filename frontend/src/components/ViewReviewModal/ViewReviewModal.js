1a8a6b07d5627f89bf1fdace0da6b47dea0b9080
Jia Huang <jiahuang2468@gmail.com>
2023-05-05 20:31:06 -0400

    purchase

--- frontend/src/components/ViewReviewModal/ViewReviewModal.js
+++ frontend/src/components/ViewReviewModal/ViewReviewModal.js
@@ -1,11 +1,13 @@
-import { useRef, useEffect, useContext } from "react";
+import { useRef, useEffect, useContext, useState } from "react";
 import Ratings from "../Flight/Ratings/Ratings";
 import styles from "./ViewReviewModal.module.css";
 import AuthContext from "../../context/auth-context";
 
 const ViewReviewModal = (props) => {
-  const ctx = useContext(AuthContext)
+  const ctx = useContext(AuthContext);
   const modalRef = useRef(null);
+  const [rating, setRating] = useState("");
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     const clickOutsideHandler = (event) => {
@@ -20,11 +22,11 @@ const ViewReviewModal = (props) => {
   }, [modalRef]);
 
   const viewReview = () => {
-    const formData = new URLSearchParams() 
-    formData.append("email", ctx.isLoggedIn.email)
-    formData.append("ticket_id", props.ticket_id)
+    const formData = new URLSearchParams();
+    formData.append("customer_email", ctx.isLoggedIn.email);
+    formData.append("ticket_id", props.ticket_id);
 
-    fetch("http://localhost:5000/rate_comment", {
+    fetch("http://localhost:5000/view_rate_comment", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: formData.toString(),
@@ -33,24 +35,25 @@ const ViewReviewModal = (props) => {
         if (response.ok) {
           return response.json();
         } else {
-          throw new Error("Error reviewing");
+          throw new Error("Error viewing review");
         }
       })
       .then((data) => {
-        
+        console.log(data)
+        setRating(data.rating);
+        setComment(data.comment);
       })
       .catch((error) => {
         console.log(error);
       });
-  }
+  };
 
-  useEffect(() => (
-    viewReview()
-  ), [])
+  useEffect(() => viewReview(), []);
 
   return (
     <div className={styles.modal} ref={modalRef}>
-      <Ratings />
+      {rating == null && comment == null && <p>No reviews</p>}
+      <Ratings rating={rating} review={comment} />
       <button onClick={props.modalHandler}>Ok</button>
     </div>
   );
